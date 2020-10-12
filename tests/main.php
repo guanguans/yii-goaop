36ffbea10510e45d1daa1474dbbcda192aaeec4f
guanguans <yzmguanguan@gmail.com>
2020-10-12 10:35:32 +0800

    Building a basic skeleton

---
+++ tests/main.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the guanguans/yii-goaop.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+$config = [
+    'id' => 'yii-goaop',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => [
+    ],
+    'modules' => [
+    ],
+    'components' => [
+    ],
+];
+
+return $config;
